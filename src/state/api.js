031ffb3269a5c3b4ba79cc8c91413a19265b1efd
heep-jay <jubrilmuritala69@gmail.com>
2023-05-06 09:35:11 -0500

    hey

--- src/state/api.js
+++ src/state/api.js
@@ -3,10 +3,9 @@ import { json } from "react-router-dom";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      "https://wealthgo.onrender.com/" ||
-      "http://localhost:5000/" ||
-      process.env.REACT_APP_BASE_URL,
+    baseUrl: "https://wealthgo.onrender.com/",
+    // "http://localhost:5000/" ||
+    // process.env.REACT_APP_BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       // By default, if we have a token in the store, let's use that for authenticated requests
       let token = localStorage.getItem("persist:root");
